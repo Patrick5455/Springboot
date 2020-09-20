12ef49ebf334d7aa8e9ad71435759625c825cd12
Patrick5455 <patrickojunde@gmail.com>
2020-09-20 15:34:40 +0100

    adding constructor dependency injection

--- com.conferenceApp-java-config/src/main/java/com/conferecne/service/SpeakerServiceImpl.java
+++ com.conferenceApp-java-config/src/main/java/com/conferecne/service/SpeakerServiceImpl.java
@@ -8,15 +8,15 @@ import java.util.List;
 
 public class SpeakerServiceImpl implements SpeakerService {
 
+    private SpeakerRepo speakerRepo;
+
+
     public SpeakerServiceImpl(){}
 
     // to be used for constructor bean dependency injection
     public SpeakerServiceImpl(SpeakerRepo speakerRepo){
         this.speakerRepo = speakerRepo;
     }
-
-    private SpeakerRepo speakerRepo;
-
 
     // to be used for setter bean dependency injection
     public void setSpeakerRepo(SpeakerRepo speakerRepo) {
