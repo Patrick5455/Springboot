48105e279e1a75f1fd37eab64db9ac04c42990fb
Patrick5455 <patrickojunde@gmail.com>
2020-09-20 15:25:56 +0100

    added javaconfig class to the Spring ApplicationContext

--- com.conferenceApp-java-config/src/main/java/ConferenceApplication.java
+++ com.conferenceApp-java-config/src/main/java/ConferenceApplication.java
@@ -1,6 +1,8 @@
 import com.conferecne.model.Speaker;
 import com.conferecne.service.SpeakerService;
 import com.conferecne.service.SpeakerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.List;
 
@@ -8,7 +10,20 @@ public class ConferenceApplication {
 
     public static void main(String[] args) {
 
-        SpeakerService service = new SpeakerServiceImpl();
+        // add java configuration to the Spring Application context
+        ApplicationContext applicationContext = new
+                AnnotationConfigApplicationContext(AppConfig.class);
+
+        // when we run the main application, spring will startup and create
+        // a registry of the configured beans viz
+        // speakerService beans and speakerRepo beans
+        // (also injects the speakerRepo bean into the SpeakerService bean as configured in the
+        // app config class)
+
+
+
+        SpeakerService service = applicationContext.getBean(
+                "speakerService", SpeakerService.class);
 
         System.out.println(service.findAll().get(0).getFirstName());
 
