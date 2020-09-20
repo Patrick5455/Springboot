d504557592694ca1b8ce655ad1e07e6ca22c7781
Patrick5455 <patrickojunde@gmail.com>
2020-09-20 19:47:32 +0100

    FactoryBean

---
+++ com.conferenceApp-no-config/src/main/java/com/conferecne/util/CalendarFactory.java
@@ -0,0 +1,19 @@
+package com.conferecne.util;
+
+import org.springframework.beans.factory.FactoryBean;
+
+import java.util.Calendar;
+
+public class CalendarFactory implements FactoryBean<Calendar> {
+
+
+    @Override
+    public Calendar getObject() throws Exception {
+        return null;
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+        return null;
+    }
+}
