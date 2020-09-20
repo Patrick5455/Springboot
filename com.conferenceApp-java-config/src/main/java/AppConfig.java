4cc6be2458935d1636916ca395386b10a81bf850
Patrick5455 <patrickojunde@gmail.com>
2020-09-20 15:19:21 +0100

    added configuration to create beans for SpearkerRepo and SpeakerService and autowire speakerRepo into speakerRepo

---
+++ com.conferenceApp-java-config/src/main/java/AppConfig.java
@@ -0,0 +1,27 @@
+import com.conferecne.repo.SpeakerRepo;
+import com.conferecne.repo.speakerRepoImpl;
+import com.conferecne.service.SpeakerService;
+import com.conferecne.service.SpeakerServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean(name = "speakerService")
+    public SpeakerService getSpeakerService(){
+        SpeakerServiceImpl speakerService = new SpeakerServiceImpl();
+
+        // autowire bean of SpeakerRepo into bean of SpeakerService
+        speakerService.setSpeakerRepo(getSpeakerRepo());
+
+        return new SpeakerServiceImpl();
+    }
+
+
+    @Bean(name = "speakerRepo")
+    public SpeakerRepo getSpeakerRepo(){
+        return new speakerRepoImpl();
+    }
+
+}
--- com.conferenceApp-java-config/src/main/java/com/conferecne/service/SpeakerServiceImpl.java
+++ com.conferenceApp-java-config/src/main/java/com/conferecne/service/SpeakerServiceImpl.java
@@ -8,10 +8,19 @@ import java.util.List;
 
 public class SpeakerServiceImpl implements SpeakerService {
 
-    private SpeakerRepo speakerRepo = new speakerRepoImpl();
+    private SpeakerRepo speakerRepo;
+
+    public SpeakerRepo getSpeakerRepo() {
+        return speakerRepo;
+    }
+
+    public void setSpeakerRepo(SpeakerRepo speakerRepo) {
+        this.speakerRepo = speakerRepo;
+    }
 
     public List<Speaker> findAll () {
         return speakerRepo.findAll();
     }
+
 
 }
