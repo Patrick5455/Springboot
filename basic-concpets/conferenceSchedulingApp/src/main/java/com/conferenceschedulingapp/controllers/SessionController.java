d4e0a47eb649492bd66a59a679a3893f4c68777e
Patrick5455 <patrickojunde@gmail.com>
2020-09-24 00:50:52 +0100

    Added session controllers endpoints

--- basic-concpets/conferenceSchedulingApp/src/main/java/com/conferenceschedulingapp/controllers/SessionController.java
+++ basic-concpets/conferenceSchedulingApp/src/main/java/com/conferenceschedulingapp/controllers/SessionController.java
@@ -1,12 +1,34 @@
 package com.conferenceschedulingapp.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.conferenceschedulingapp.models.Session;
+import com.conferenceschedulingapp.repositories.SessionRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/sessions")
 public class SessionController {
 
+    @Autowired
+    private SessionRepo sessionRepo;
+
+    @GetMapping
+    public List<Session> listSession(){
+        return sessionRepo.findAll();
+    }
+
+    @GetMapping
+    @RequestMapping("{id")
+    public Session getSession(@PathVariable Long id){
+        return sessionRepo.getOne(id);
+    }
+
+    @PostMapping
+    public Session createSession(@RequestBody final Session session){
+        return sessionRepo.saveAndFlush(session);
+    }
 
 
 }
