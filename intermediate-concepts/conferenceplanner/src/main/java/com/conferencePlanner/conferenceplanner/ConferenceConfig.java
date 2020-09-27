2e08b67a58c79172c2d08cfb335af011208e6e2b
Patrick5455 <patrickojunde@gmail.com>
2020-09-27 22:30:31 +0100

    Implemented Spring app.prop config View Resolver as Java config class

---
+++ intermediate-concepts/conferenceplanner/src/main/java/com/conferencePlanner/conferenceplanner/ConferenceConfig.java
@@ -0,0 +1,21 @@
+package com.conferencePlanner.conferenceplanner;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+public class ConferenceConfig {
+
+    // implementing the view resolver configured in application properties
+    @Bean
+    public ViewResolver viewResolver(){
+        InternalResourceViewResolver bean = new InternalResourceViewResolver();
+        bean.setPrefix("/WEB-INF/jsp/");
+        bean.setSuffix(".jsp");
+        bean.setOrder(0);
+        return bean;
+    }
+
+}
