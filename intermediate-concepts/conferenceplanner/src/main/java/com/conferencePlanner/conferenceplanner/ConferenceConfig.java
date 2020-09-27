4d271593e85c15dfc8611d389997003b6bc2f332
Patrick5455 <patrickojunde@gmail.com>
2020-09-27 22:37:01 +0100

    Added Resource Handler to handle files like pdf

--- intermediate-concepts/conferenceplanner/src/main/java/com/conferencePlanner/conferenceplanner/ConferenceConfig.java
+++ intermediate-concepts/conferenceplanner/src/main/java/com/conferencePlanner/conferenceplanner/ConferenceConfig.java
@@ -3,10 +3,21 @@ package com.conferencePlanner.conferenceplanner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
-public class ConferenceConfig {
+public class ConferenceConfig implements WebMvcConfigurer {
+
+
+   @Override
+   public void addResourceHandlers(ResourceHandlerRegistry registry){
+
+       registry.addResourceHandler("/files/**")
+               .addResourceLocations("/WEB-INF/pdf/");
+   }
+
 
     // implementing the view resolver configured in application properties
     @Bean
