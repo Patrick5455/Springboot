064b37b7fbc407e19934c9b8d3289c2e78f99a7e
Patrick5455 <patrickojunde@gmail.com>
2020-09-28 00:58:49 +0100

    Added validation

--- intermediate-concepts/conferenceplanner/src/main/java/com/conferencePlanner/conferenceplanner/controllers/RegistrationController.java
+++ intermediate-concepts/conferenceplanner/src/main/java/com/conferencePlanner/conferenceplanner/controllers/RegistrationController.java
@@ -2,18 +2,26 @@ package com.conferencePlanner.conferenceplanner.controllers;
 
 import com.conferencePlanner.conferenceplanner.model.Registration;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
 import java.util.Map;
 
 @Controller
 public class RegistrationController {
 
     @GetMapping("registration")
-    public String getRegistration(@ModelAttribute("registration")
-                                              Registration registration){
+    //@Valid from hibernate-validator for validation
+    public String getRegistration(@Valid @ModelAttribute("registration")
+                                              Registration registration,
+                                  // the binding result is used to
+                                  // get any error signature passed by reference ad display it for us
+                                  BindingResult result,
+                                  ModelMap modelMap){
         return "registration";
     }
 
@@ -22,6 +30,7 @@ public class RegistrationController {
                                           Registration registration){
 
         System.out.println("Registration: " +registration.getName());
+        //Post-Redirect-Get Pattern
         // redirect to registration page
         return "redirect:registration";
     }
--- intermediate-concepts/conferenceplanner/src/main/java/com/conferencePlanner/conferenceplanner/model/Registration.java
+++ intermediate-concepts/conferenceplanner/src/main/java/com/conferencePlanner/conferenceplanner/model/Registration.java
@@ -1,7 +1,12 @@
 package com.conferencePlanner.conferenceplanner.model;
 
+import org.springframework.lang.NonNull;
+
+import javax.validation.constraints.NotEmpty;
+
 public class Registration {
 
+    @NotEmpty(message = "Cannot be empty")
     private String name;
 
     public String getName() {
