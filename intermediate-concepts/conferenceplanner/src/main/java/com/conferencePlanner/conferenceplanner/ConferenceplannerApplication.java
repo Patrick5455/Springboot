1693fc788fa4a92e3837b71fef103f4b08f23af7
Patrick5455 <patrickojunde@gmail.com>
2020-09-27 14:29:17 +0100

    Add first intermediate project

---
+++ intermediate-concepts/conferenceplanner/src/main/java/com/conferencePlanner/conferenceplanner/ConferenceplannerApplication.java
@@ -0,0 +1,13 @@
+package com.conferencePlanner.conferenceplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferenceplannerApplication.class, args);
+	}
+
+}
---
+++ intermediate-concepts/conferenceplanner/src/test/java/com/conferencePlanner/conferenceplanner/ConferenceplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conferencePlanner.conferenceplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
