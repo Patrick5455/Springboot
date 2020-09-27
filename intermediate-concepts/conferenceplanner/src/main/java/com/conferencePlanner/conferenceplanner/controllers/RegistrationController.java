bb3fbe84d6c676300ece5538845d78443ca3d19b
Patrick5455 <patrickojunde@gmail.com>
2020-09-27 22:18:25 +0100

    Added Post Mapping for registration

--- intermediate-concepts/conferenceplanner/src/main/java/com/conferencePlanner/conferenceplanner/controllers/RegistrationController.java
+++ intermediate-concepts/conferenceplanner/src/main/java/com/conferencePlanner/conferenceplanner/controllers/RegistrationController.java
@@ -4,6 +4,7 @@ import com.conferencePlanner.conferenceplanner.model.Registration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Map;
 
@@ -13,9 +14,16 @@ public class RegistrationController {
     @GetMapping("registration")
     public String getRegistration(@ModelAttribute("registration")
                                               Registration registration){
-
         return "registration";
     }
+
+    @PostMapping("registration")
+    public String addRegistration(@ModelAttribute("registration")
+                                          Registration registration){
+        System.out.println("Registration: " +registration.getName());
+        return "registration";
+    }
+
 
 
 //    public String getRegistration(Map<String, Object> model){
