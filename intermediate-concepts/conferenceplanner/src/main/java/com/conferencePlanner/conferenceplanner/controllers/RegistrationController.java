a316c48105393d26a52d044d5bd0ae5efff06003
Patrick5455 <patrickojunde@gmail.com>
2020-09-27 22:15:04 +0100

    Added Registration model

--- intermediate-concepts/conferenceplanner/src/main/java/com/conferencePlanner/conferenceplanner/controllers/RegistrationController.java
+++ intermediate-concepts/conferenceplanner/src/main/java/com/conferencePlanner/conferenceplanner/controllers/RegistrationController.java
@@ -1,7 +1,9 @@
 package com.conferencePlanner.conferenceplanner.controllers;
 
+import com.conferencePlanner.conferenceplanner.model.Registration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import java.util.Map;
 
@@ -9,8 +11,15 @@ import java.util.Map;
 public class RegistrationController {
 
     @GetMapping("registration")
-    public String getRegistration(Map<String, Object> model){
-        model.put("message", "Register Here");
+    public String getRegistration(@ModelAttribute("registration")
+                                              Registration registration){
+
         return "registration";
     }
+
+
+//    public String getRegistration(Map<String, Object> model){
+//        model.put("message", "Register Here");
+//        return "registration";
+//    }
 }
--- intermediate-concepts/conferenceplanner/src/main/webapp/WEB-INF/jsp/registration.jsp
+++ intermediate-concepts/conferenceplanner/src/main/webapp/WEB-INF/jsp/registration.jsp
@@ -6,9 +6,27 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%--jsp tags for forms--%>
+<%@taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
+
 <html>
 <head>
     <title>Registration</title>
+    <form:form modelAttribute="registration">
+        <table>
+            <tr>
+                <td>Name:</td>
+                <td><form:input path="name"/></td>
+            </tr>
+            <td colspan="2">
+                <input type="submit" value="Add Registration">
+            </td>
+            <tr>
+            </tr>
+        </table>
+    </form:form>
+
+
 </head>
 <body>
 
