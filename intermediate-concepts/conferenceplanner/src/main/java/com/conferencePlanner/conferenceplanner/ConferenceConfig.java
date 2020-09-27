73031dd603f9980e89299a4b451990fdeee07ca7
Patrick5455 <patrickojunde@gmail.com>
2020-09-27 22:44:51 +0100

    Added comments in the usecase of ResourceHandler

--- intermediate-concepts/conferenceplanner/src/main/java/com/conferencePlanner/conferenceplanner/ConferenceConfig.java
+++ intermediate-concepts/conferenceplanner/src/main/java/com/conferencePlanner/conferenceplanner/ConferenceConfig.java
@@ -11,9 +11,15 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 public class ConferenceConfig implements WebMvcConfigurer {
 
 
+    // configuring springboot to host up/serve up our pdf files
+    // this helps us to create custom paths(/files/**) that
+    // goes to a different place on our file system
+    // this we would not have to expose files of our file-system
+    // but rather could host them up through our app
+    // or rather even wrap security around them if we wanted to
+
    @Override
    public void addResourceHandlers(ResourceHandlerRegistry registry){
-
        registry.addResourceHandler("/files/**")
                .addResourceLocations("/WEB-INF/pdf/");
    }
